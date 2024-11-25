ef9d2e7d7c8489242d86328415941fa10c8ffc3d
Rami1x <rami.ayourama@gmail.com>
2024-11-25 01:13:40 -0700

    CHANGED EVERYTHING

--- main.py
+++ main.py
@@ -1,183 +1,182 @@
+# <------------------------------ Imports -------------------------------->
 import sys
-
-x = '192.168.10.1'
-y = '192.168.20.1'
+# <------------------------------ Functions ------------------------------>
 
 def check(ipOne, ipTwo):
-    legal = ["1", "2", "3",
-        "4", "5", "6",
-        "7", "8", "9", "0", "."]
+    illegal = 0
+    forbidden = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."]
+    i = 0
+    result = True
 
-    for num1, num2 in zip(ipOne, ipTwo):    # this for loop checks if its in IPV4 format.
-        if num1 in legal and num2 in legal:
-            result = True
+    while i < len(ipOne):
+        if ipOne[i] in forbidden:
+            i += 1
         else:
-            result = False
-            break
+            illegal += 1
+            i += 1
+    i = 0
+
+    while i < len(ipTwo):
+        if ipTwo[i] in forbidden:
+            i += 1
+        else:
+            illegal += 1
+            i += 1
     
+    match illegal:
+        case 0:
+            result = True
+        case _:
+            result = False
+
     return result
 
 def segmentation(ipOne, ipTwo):
-    dot = "."
-    sim_ip = ""
-    sim_ip_append = []
-    wrong_ip_line = 0
-    not_ip1 = ""
-    not_ip2 = ""
-    not_ip1_append = []
-    not_ip2_append = []
+    seg_split = ""
+    segment1 = []
+    segment2 = []
 
-    for num1, num2 in zip(ipOne, ipTwo): # this is the for loop which finds similar ip addresses but checks if its full segment similarity first
-        if num1 != dot and num2 != dot:
-            if num1 == num2:
-                sim_ip += num1
-                wrong_ip_line += 1
-            else:
-                break
-        elif num2 == dot and num2 == dot:
-            sim_ip_append.append(sim_ip)
-            wrong_ip_line += 1
-            sim_ip = ""
+    for num in ipOne:
+        if num != ".":
+            seg_split += num
         else:
-            break
+            segment1.append(seg_split)
+            seg_split = ""
 
-    for i in range(wrong_ip_line, len(ipOne)):
-        num1 = ipOne[i]
-        num2 = ipTwo[i]
-        if num1 != dot and num2 != dot:
-            not_ip1 += num1
-            not_ip2 += num2
-        elif num1 == dot and num2 == dot:
-            not_ip1_append.append(not_ip1)
-            not_ip2_append.append(not_ip2)
-            not_ip1 = ""
-            not_ip2 = ""
+    segment1.append(seg_split)
+    seg_split = ""
+
+    for num in ipTwo:
+        if num != ".":
+            seg_split += num
         else:
-            break
-
-    if not_ip1 or not_ip2:
-        not_ip1_append.append(not_ip1)
-        not_ip2_append.append(not_ip2)
-
-
-    return sim_ip_append, not_ip1_append, not_ip2_append
-
-def binary(diffOne, diffTwo):
-    diffOne_int = []
-    diffTwo_int = []
-    num_list = [128, 64, 32, 16, 8, 4, 2, 1]
-    binary_diffOne = ""
-    binary_diffTwo = ""
-    binary_one_append = []
-    binary_two_append = []
-
-    for val in diffOne:
-        diffOne_int.append(int(val))
-    for val in diffTwo:
-        diffTwo_int.append(int(val))
-
-    for num in diffOne_int:
-        binary_diffOne = ""
-        for bit in num_list:
-            if num >= bit:
-                binary_diffOne += "1"
-                num -= bit
-            else:
-                binary_diffOne += "0"
-        binary_one_append.append(binary_diffOne)
-
-    for numb in diffTwo_int:
-        binary_diffTwo = ""
-        for bitt in num_list:
-            if numb >= bitt:
-                binary_diffTwo += "1"
-                numb -= bitt
-            else:
-                binary_diffTwo += "0"
-
-        binary_two_append.append(binary_diffTwo)
-
-    return binary_one_append, binary_two_append
-
-def getSummarized(One, Two):
-    sum_append = []
-    rowOne = []
-    rowTwo = []
-    summarized = ""
-
-    rowOne.append(One[0])
-    rowOne.append(Two[0])
-    rowTwo.append(One[1])
-    rowTwo.append(Two[1])
-
-    for num1, num2 in zip(rowOne[0], rowOne[1]):
-        if num1 != num2:
-            summarized += "1"
-        else:
-            if num1 == "0":
-                summarized += "0"
-            else:
-                summarized = summarized[:-1]
-                summarized += "10"
-
-    sum_append.append(summarized)
-    summarized = ""
-
-    for num1, num2 in zip(rowTwo[0], rowTwo[1]):
-        if num1 != num2:
-            summarized += "1"
-        else:
-            if num1 == "0":
-                summarized += "0"
-            else:
-                summarized = summarized[:-1]
-                summarized += "10"
+            segment2.append(seg_split)
+            seg_split = ""
+    segment2.append(seg_split)
     
-    sum_append.append(summarized)
+    return segment1, segment2
 
-    return sum_append
-
-def ipv4_output(beginning, ending):
+def Binary_Conversion(IP1, IP2):
     num_list = [128, 64, 32, 16, 8, 4, 2, 1]
-    ipv4_1 = 0
-    ipv4_2 = 0
-    i = 0
-    final_value = ""
+    IP1_int = []
+    IP2_int = []
+    binary_str = ""
+    binary_append = [] 
+    binary_append2 = [] 
 
-    for num in ending[0]:
-        if num == "1":
-            ipv4_1 += num_list[i]
-            i += 1
+
+    for inst in IP1:
+        IP1_int.append(int(inst))
+
+    for inst in IP2:
+        IP2_int.append(int(inst))
+
+    for val in IP1_int:
+        for number in num_list:
+            if val >= number:
+                binary_str += "1"
+                val = val - number
+            else:
+                binary_str += "0"
+        binary_append.append(binary_str)
+        binary_str = ""
+
+    for val1 in IP2_int:
+        binary_str = ""
+        for number in num_list:
+            if val1 >= number:
+                binary_str += "1"
+                val1 = val1 - number
+            else:
+                binary_str += "0"
+        binary_append2.append(binary_str)
+                
+    
+    return binary_append, binary_append2
+
+def Binary_Compare(b_one, b_two):
+    same_binary = ""
+    same_binary_append = []
+    binary_str1 = ""
+    binary_str2 = ""
+    i = 0
+    i2 = 0
+
+    for num1, num2 in zip(b_one, b_two):
+        binary_str1 += num1
+        binary_str2 += num2
+
+    for num1, num2 in zip(binary_str1, binary_str2):
+        if num1 == num2:
+            same_binary += num1
         else:
-            i += 1
+            break
+    
+    subnet = f"/{len(same_binary)}"
 
     i = 0
 
-    for num in ending[1]:
-        if num == "1":
-            ipv4_2 += num_list[i]
-            i += 1
+    while i < len(same_binary):
+        chunk = same_binary[i:i+8]
+        if len(chunk) < 8:
+            chunk = chunk.ljust(8, "0")
+        same_binary_append.append(chunk)
+        i += 8
+
+    return same_binary_append, subnet
+
+def Conclusion(summary, sub_mask):
+    num_list = [128, 64, 32, 16, 8, 4, 2, 1]
+    sum = 0
+    ip_append = []
+    i = 0
+    j = 0
+    full_ip = ""
+
+    while j < len(summary):
+        for num in summary[j]:
+            if num == "1":
+                sum += num_list[i]
+                i += 1
+            else:
+                i += 1
+        ip_append.append(sum)
+        sum = 0
+        j += 1
+        i = 0
+
+    i = len(ip_append)
+
+    while i < 4:
+        ip_append.append(0)
+        i += 1
+    i = 0
+
+    while i < len(ip_append):
+        full_ip += f"{ip_append[i]}"
+        if i == 3:  
+            full_ip += f"{sub_mask}"
         else:
-            i += 1
+            full_ip += "." 
+        i += 1
+
+    return print(full_ip)
     
-    for value in beginning:
-        final_value +=  (value + ".")
     
-    final_value += (str(ipv4_1) + "." + str(ipv4_2))
+# <-------------------------------- Main -------------------------------->
 
-
-
-    return print(final_value)
+x = "192.168.10.1"
+y = "192.168.20.1"
 
 checker = check(x, y) 
 
 if checker == True:
-    same_addr, diff_addr1, diff_addr2 = segmentation(x, y)
-    binary_convert_one, binary_convert_two = binary(diff_addr1, diff_addr2)
-    summarized = getSummarized(binary_convert_one, binary_convert_two)
+    print("nice")
+    first_ip, second_ip = segmentation(x, y)
+    x_binary, y_binary = Binary_Conversion(first_ip, second_ip)
+    same_binary, subnet = Binary_Compare(x_binary, y_binary)
+    Conclusion(same_binary, subnet)
 
-
-    print(f"Identical segments of first and second IP: {same_addr}\nDifferent segments for first IP: {diff_addr1}\nDifferent segments for second IP: {diff_addr2}\n")
-
-    ipv4_output(same_addr, summarized)
+else:
+    print("L")
     
